83f357beb0572d24e7f0a08dc31383cc01b81822
Troy Larson <troylars@amazon.com>
2016-12-29 09:40:08 -0500

    Initial checkin

---
+++ QuietOnTheSetUI/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace QuietOnTheSetUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.volumeTrackBar = new System.Windows.Forms.TrackBar();
+            this.lockButton = new System.Windows.Forms.Button();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.confirmPasswordTextBox = new System.Windows.Forms.TextBox();
+            this.currentVolumeLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.showPasswordCheckbox = new System.Windows.Forms.CheckBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.maxVolumeLabel = new System.Windows.Forms.Label();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.volumeTrackBar)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // volumeTrackBar
+            // 
+            this.volumeTrackBar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.volumeTrackBar.Location = new System.Drawing.Point(0, 33);
+            this.volumeTrackBar.Maximum = 100;
+            this.volumeTrackBar.Name = "volumeTrackBar";
+            this.volumeTrackBar.Size = new System.Drawing.Size(767, 45);
+            this.volumeTrackBar.TabIndex = 0;
+            this.volumeTrackBar.Scroll += new System.EventHandler(this.volumeTrackBar_Scroll);
+            // 
+            // lockButton
+            // 
+            this.lockButton.Font = new System.Drawing.Font("Calibri", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lockButton.Location = new System.Drawing.Point(284, 264);
+            this.lockButton.Name = "lockButton";
+            this.lockButton.Size = new System.Drawing.Size(201, 72);
+            this.lockButton.TabIndex = 2;
+            this.lockButton.Text = "Lock It";
+            this.lockButton.UseVisualStyleBackColor = true;
+            this.lockButton.Click += new System.EventHandler(this.lockButton_Click);
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Font = new System.Drawing.Font("Century Schoolbook", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordTextBox.Location = new System.Drawing.Point(17, 251);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(224, 33);
+            this.passwordTextBox.TabIndex = 3;
+            this.passwordTextBox.UseSystemPasswordChar = true;
+            this.passwordTextBox.TextChanged += new System.EventHandler(this.passwordTextBox_TextChanged);
+            // 
+            // confirmPasswordTextBox
+            // 
+            this.confirmPasswordTextBox.Font = new System.Drawing.Font("Century Schoolbook", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.confirmPasswordTextBox.Location = new System.Drawing.Point(17, 322);
+            this.confirmPasswordTextBox.Name = "confirmPasswordTextBox";
+            this.confirmPasswordTextBox.Size = new System.Drawing.Size(224, 33);
+            this.confirmPasswordTextBox.TabIndex = 4;
+            this.confirmPasswordTextBox.UseSystemPasswordChar = true;
+            this.confirmPasswordTextBox.TextChanged += new System.EventHandler(this.confirmPasswordTextBox_TextChanged);
+            // 
+            // currentVolumeLabel
+            // 
+            this.currentVolumeLabel.Font = new System.Drawing.Font("Calibri", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.currentVolumeLabel.Location = new System.Drawing.Point(-1, 0);
+            this.currentVolumeLabel.Name = "currentVolumeLabel";
+            this.currentVolumeLabel.Size = new System.Drawing.Size(187, 59);
+            this.currentVolumeLabel.TabIndex = 5;
+            this.currentVolumeLabel.Text = "100";
+            this.currentVolumeLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 222);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(266, 26);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Lock Password (not required):";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 293);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(172, 26);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Confirm Password:";
+            // 
+            // showPasswordCheckbox
+            // 
+            this.showPasswordCheckbox.AutoSize = true;
+            this.showPasswordCheckbox.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.showPasswordCheckbox.Location = new System.Drawing.Point(76, 363);
+            this.showPasswordCheckbox.Name = "showPasswordCheckbox";
+            this.showPasswordCheckbox.Size = new System.Drawing.Size(165, 30);
+            this.showPasswordCheckbox.TabIndex = 8;
+            this.showPasswordCheckbox.Text = "Show Password";
+            this.showPasswordCheckbox.UseVisualStyleBackColor = true;
+            this.showPasswordCheckbox.CheckedChanged += new System.EventHandler(this.showPasswordCheckbox_CheckedChanged);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.Font = new System.Drawing.Font("Century Schoolbook", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(2, 7);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(765, 38);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Slide this to pick the maximum volume";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Century Schoolbook", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(-1, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(187, 25);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Current Volume";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.currentVolumeLabel);
+            this.panel1.Location = new System.Drawing.Point(167, 84);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(187, 96);
+            this.panel1.TabIndex = 11;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.maxVolumeLabel);
+            this.panel2.Location = new System.Drawing.Point(422, 84);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(187, 96);
+            this.panel2.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.Font = new System.Drawing.Font("Century Schoolbook", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(-1, 59);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(187, 25);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Max Volume";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // maxVolumeLabel
+            // 
+            this.maxVolumeLabel.Font = new System.Drawing.Font("Calibri", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maxVolumeLabel.Location = new System.Drawing.Point(-1, 0);
+            this.maxVolumeLabel.Name = "maxVolumeLabel";
+            this.maxVolumeLabel.Size = new System.Drawing.Size(187, 59);
+            this.maxVolumeLabel.TabIndex = 5;
+            this.maxVolumeLabel.Text = "100";
+            this.maxVolumeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("Calibri", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(536, 264);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(201, 72);
+            this.exitButton.TabIndex = 13;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(365, 363);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(272, 30);
+            this.checkBox1.TabIndex = 14;
+            this.checkBox1.Text = "Start automatically on login";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(763, 425);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.showPasswordCheckbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.confirmPasswordTextBox);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.lockButton);
+            this.Controls.Add(this.volumeTrackBar);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Quiet on the Set";
+            ((System.ComponentModel.ISupportInitialize)(this.volumeTrackBar)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar volumeTrackBar;
+        private System.Windows.Forms.Button lockButton;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.TextBox confirmPasswordTextBox;
+        private System.Windows.Forms.Label currentVolumeLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox showPasswordCheckbox;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label maxVolumeLabel;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ QuietOnTheSetUI/Form1.cs
@@ -0,0 +1,236 @@
+﻿using Microsoft.Win32;
+using NAudio.CoreAudioApi;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuietOnTheSetUI
+{
+    public partial class Form1 : Form
+    {
+        MMDeviceEnumerator MMDE = new MMDeviceEnumerator();
+        MMDevice mmDevice;
+        private bool _isLocked = false;
+        private string _password;
+        private int _maxVolume;
+        private bool _exitAllowed = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Bitmap applicationIcon = QuietOnTheSetUI.Properties.Resources.appicon;
+            this.Icon = Icon.FromHandle(applicationIcon.GetHicon());
+            mmDevice = MMDE.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+            notifyIcon1.Icon = Icon.FromHandle(applicationIcon.GetHicon());
+            volumeTrackBar.ValueChanged += VolumeTrackBar_ValueChanged;
+            mmDevice.AudioEndpointVolume.OnVolumeNotification += AudioEndpointVolume_OnVolumeNotification;
+            _maxVolume = Convert.ToInt16(Properties.Settings.Default["MaxVolume"]);
+            volumeTrackBar.Value = _maxVolume;
+            _isLocked = Convert.ToBoolean(Properties.Settings.Default["IsLocked"]);
+            _password = Properties.Settings.Default["UnlockCode"].ToString();
+            currentVolumeLabel.Text = Convert.ToInt16(mmDevice.AudioEndpointVolume.MasterVolumeLevelScalar * 100).ToString();
+            notifyIcon1.BalloonTipTitle = $"Quiet on the Set";
+            if (_isLocked)
+            {
+                LockVolume(true);
+            }
+            else
+            {
+                UnlockVolume();
+            }
+
+            this.FormClosing += Form1_FormClosing;
+            this.Resize += Form1_Resize;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+                notifyIcon1.Visible = true;
+                notifyIcon1.ShowBalloonTip(500);
+                this.Hide();
+            }
+            else if (FormWindowState.Normal == this.WindowState)
+            {
+                notifyIcon1.Visible = false;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (_exitAllowed == false)
+            {
+                e.Cancel = true;
+                this.WindowState = FormWindowState.Minimized;
+                this.ShowInTaskbar = false;
+                Hide();
+            }
+        }
+
+        internal void LockVolume (bool initializing=false)
+        {
+            _isLocked = true;
+            lockButton.Text = "Unlock";
+            volumeTrackBar.Enabled = false;
+            if (!initializing)
+            {
+                _maxVolume = volumeTrackBar.Value;
+                Properties.Settings.Default["MaxVolume"] = _maxVolume.ToString();
+                Properties.Settings.Default["IsLocked"] = true;
+                Properties.Settings.Default["UnlockCode"] = passwordTextBox.Text;
+                Properties.Settings.Default.Save();
+            }
+            _password = passwordTextBox.Text;
+            passwordTextBox.Text = string.Empty;
+            confirmPasswordTextBox.Text = string.Empty;
+            notifyIcon1.BalloonTipText = $"Maximum volume locked at {volumeTrackBar.Value}";
+            if (_password.Length > 0) { lockButton.Enabled = false; }
+            exitButton.Visible = false;
+            SetMaxVolume();
+        }
+        internal void UnlockVolume()
+        {
+            _isLocked = false;
+            lockButton.Text = "Lock";
+            volumeTrackBar.Enabled = true;
+            Properties.Settings.Default["IsLocked"] = false;
+            Properties.Settings.Default["UnlockCode"] = string.Empty;
+            Properties.Settings.Default.Save();
+            passwordTextBox.Text = string.Empty;
+            confirmPasswordTextBox.Text = string.Empty;
+            notifyIcon1.BalloonTipText = $"No maximum volume is current set";
+            exitButton.Visible = true;
+            _password = string.Empty;
+        }
+
+        private void SetMaxVolume()
+        {
+            if (mmDevice.AudioEndpointVolume.MasterVolumeLevelScalar > (_maxVolume / 100f))
+            {
+                mmDevice.AudioEndpointVolume.MasterVolumeLevelScalar = _maxVolume / 100f;
+            }
+        }
+
+        private void AudioEndpointVolume_OnVolumeNotification(AudioVolumeNotificationData data)
+        {
+            var newVolume = Convert.ToInt16(data.MasterVolume * 100);
+            if (_isLocked && newVolume >  _maxVolume)
+            {
+                SetMaxVolume();
+            }
+            if (currentVolumeLabel.InvokeRequired)
+            {
+                currentVolumeLabel.Invoke(new MethodInvoker(delegate { currentVolumeLabel.Text = newVolume.ToString() ; }));
+            }
+        }
+
+        private void VolumeTrackBar_ValueChanged(object sender, EventArgs e)
+        {
+            maxVolumeLabel.Text = volumeTrackBar.Value.ToString();
+        }
+
+        private void volumeTrackBar_Scroll(object sender, EventArgs e)
+        {
+            var trackBar = (TrackBar)sender;
+            maxVolumeLabel.Text = trackBar.Value.ToString();
+        }
+
+        private void lockButton_Click(object sender, EventArgs e)
+        {
+            if (_isLocked)
+            {
+                UnlockVolume();
+            }
+            else
+            {
+                LockVolume();
+            }
+        }
+
+        private void passwordTextBox_TextChanged(object sender, EventArgs e)
+        {
+            ValidatePasswords();
+        }
+
+        private void confirmPasswordTextBox_TextChanged(object sender, EventArgs e)
+        {
+            ValidatePasswords();
+        }
+
+        internal void ValidatePasswords()
+        {
+            if (_isLocked)
+            {
+                lockButton.Enabled = passwordTextBox.Text.Equals(confirmPasswordTextBox.Text) && passwordTextBox.Text.Equals(_password);
+            }
+            else
+            {
+                lockButton.Enabled = passwordTextBox.Text.Equals(confirmPasswordTextBox.Text);
+            }
+        }
+
+        private void showPasswordCheckbox_CheckedChanged(object sender, EventArgs e)
+        {
+            var isChecked = ((CheckBox)sender).Checked;
+            passwordTextBox.UseSystemPasswordChar = !isChecked;
+            confirmPasswordTextBox.UseSystemPasswordChar = !isChecked;
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            Form1 frm = new Form1();
+            frm.Show();
+            MaxmizedFromTray();
+        }
+        private void MinimzedTray()
+        {
+            notifyIcon1.Visible = true;
+            notifyIcon1.Icon = SystemIcons.Application;
+
+            notifyIcon1.BalloonTipText = "Minimized";
+            notifyIcon1.BalloonTipTitle = "Your Application is Running in BackGround";
+            notifyIcon1.ShowBalloonTip(500);
+        }
+
+        private void MaxmizedFromTray()
+        {
+            notifyIcon1.Visible = true;
+            notifyIcon1.BalloonTipText = "Maximized";
+            notifyIcon1.BalloonTipTitle = "Application is Running in Foreground";
+            notifyIcon1.ShowBalloonTip(500);
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            var response = MessageBox.Show("This will completely shut down the volume control so the . Are you sure you want to exit?", "Warning", MessageBoxButtons.YesNo);
+            if (response == DialogResult.Yes)
+            {
+                _exitAllowed = true;
+                Application.Exit();
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            RegistryKey rk = Registry.CurrentUser.OpenSubKey
+                ("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+            if (checkBox1.Checked)
+            {
+                rk.SetValue("QuietOnTheSet", Application.ExecutablePath.ToString());
+            }
+            else
+            {
+                rk.DeleteValue("QuietOnTheSet", false);
+            }
+        }
+    }
+}
